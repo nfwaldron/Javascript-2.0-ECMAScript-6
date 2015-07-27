4c89a56a112b459edf99b7f444bf7fc410df3b8e
nfwaldron <waldron.nathan@gmail.com>
2015-07-27 14:17:16 -0700

    update

---
+++ JavaScript Redux.2/script.js
@@ -0,0 +1,41 @@
+'use strict'
+/*
+try{
+	throw new Error('Something went terrible wrong');
+} 
+catch (ex)
+{ 
+	console.log('An error' + ex.message);
+}
+finally{
+	console.log('All done!');
+}*/
+
+function addIntegers(a,b)
+{
+	return a + b;
+}
+
+try{
+	addIntegers("1", 2)
+}
+
+catch{
+	console.log(a 'is not a number');
+}
+
+try{
+	addIntegers("apple")
+}
+
+catch{
+	console.log(a 'is not a number');
+}
+
+try{
+	addIntegers(3.4, 2)
+}
+
+catch{
+	console.log(a 'is not a number');
+}
