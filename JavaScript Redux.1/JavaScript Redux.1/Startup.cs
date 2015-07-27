f39e69e20812c7182008ef1ccac6623ed9b63099
nfwaldron <waldron.nathan@gmail.com>
2015-07-27 11:10:00 -0700

    Update

---
+++ JavaScript Redux.1/JavaScript Redux.1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JavaScript_Redux._1.Startup))]
+namespace JavaScript_Redux._1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
