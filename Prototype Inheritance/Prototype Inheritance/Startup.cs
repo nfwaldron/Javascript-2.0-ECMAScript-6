b4c1844ee5e59aa882f999d31d483d6318672ce1
nfwaldron <waldron.nathan@gmail.com>
2015-07-28 13:27:25 -0700

    update

---
+++ Prototype Inheritance/Prototype Inheritance/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Prototype_Inheritance.Startup))]
+namespace Prototype_Inheritance
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
