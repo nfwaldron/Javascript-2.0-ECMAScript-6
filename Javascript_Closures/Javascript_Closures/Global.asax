ef4c58ba4fd345987c148ca283b424af647e1f06
nfwaldron <waldron.nathan@gmail.com>
2015-07-28 11:38:49 -0700

    update

---
+++ Javascript_Closures/Javascript_Closures/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Javascript_Closures.MvcApplication" Language="C#" %>
---
+++ Javascript_Closures/Javascript_Closures/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Javascript_Closures.Startup))]
+namespace Javascript_Closures
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
