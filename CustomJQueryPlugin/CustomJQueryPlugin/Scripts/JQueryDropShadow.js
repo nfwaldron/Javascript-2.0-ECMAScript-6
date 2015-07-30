61766f48a945c0af6159f622321ef2d76a362bc3
nfwaldron <waldron.nathan@gmail.com>
2015-07-30 16:15:28 -0700

    update

---
+++ CustomJQueryPlugin/CustomJQueryPlugin/Scripts/JQueryDropShadow.js
@@ -0,0 +1,14 @@
+﻿(function ($) {
+    $.fn.dropShadow = function ()
+    {
+        this.on('click', function (e)
+        {
+            //debugger
+            // You can wrap any element and transform it into a 
+            // jQuery wrapset.
+            $(e.target).css('boxShadow', '50px 50px 50px 5px black');
+        })
+
+        return this;
+    }
+})(jQuery);
---
+++ CustomJQueryPlugin/CustomJQueryPlugin/Scripts/jQuery.rotate.js
@@ -0,0 +1,16 @@
+﻿(function ($) {
+    $.fn.rotate = function()
+    {
+
+        var settings = $.extend({
+            degrees: 20
+        });
+
+
+        this.click('click', function (e) {
+            e.target.style.transform = 'rotate(45deg)';
+        });
+        return this;
+    }
+
+})(jQuery);
